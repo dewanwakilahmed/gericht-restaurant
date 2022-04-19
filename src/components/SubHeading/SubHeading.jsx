ec02cbc6de6cc481d9e86da041061177e24f7154
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-04-19 15:26:59 +0600

    Completed Header

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const SubHeading = () => {
-  return <div>SubHeading</div>;
-};
+// Images
+import { images } from "../../constants";
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon_image" className="spoon__img" />
+  </div>
+);
 
 export default SubHeading;
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-import classes from "./Header.module.css";
+// CSS
+import "./Header.css";
+
+// Components
+import { SubHeading } from "../../components";
+
+// Images
+import { images } from "../../constants";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1">The Key To Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: "2rem 0" }}>
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+          volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+          aliquam amet tellus{" "}
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header_img" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
