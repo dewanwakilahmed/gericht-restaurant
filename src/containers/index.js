8639aa6748def814171c8602474dbd192b2dd1c0
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-11-15 23:33:18 +0600

    Edited Containers Index Exports

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,21 +1,21 @@
-import Header from "./Header/Header";
-import Intro from "./Intro/Intro";
-import AboutUs from "./AboutUs/AboutUs";
-import SpecialMenu from "./Menu/SpecialMenu";
-import Chef from "./Chef/Chef";
-import Laurels from "./Laurels/Laurels";
-import Gallery from "./Gallery/Gallery";
-import FindUs from "./FindUs/FindUs";
-import Footer from "./Footer/Footer";
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import FindUs from './FindUs/FindUs';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import Laurels from './Laurels/Laurels';
+import SpecialMenu from './Menu/SpecialMenu';
 
 export {
-  Header,
-  Intro,
   AboutUs,
-  SpecialMenu,
   Chef,
-  Laurels,
-  Gallery,
   FindUs,
   Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
 };
